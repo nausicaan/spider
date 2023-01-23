ea15a536fd2be0c4e35c32c95d7cc23655c9b3cf
bstuike <byron.stuike@gov.bc.ca>
2023-01-22 20:24:54 -0800

    Grow the switch statement

--- tasks/launch.go
+++ tasks/launch.go
@@ -16,12 +16,13 @@ type Blog struct {
 }
 
 var (
-	testList, stageList, prodList []Blog
-	testID, stageID, prodID       string
-	test, stage, prod             Blog
-
 	flag = os.Args[1]
 	site = os.Args[2]
+
+	fromPath, fromURL, toPath, toURL string
+	testID, stageID, prodID          string
+	testObj, stageObj, prodObj       Blog
+	testList, stageList, prodList    []Blog
 )
 
 /*
@@ -34,19 +35,49 @@ Flags:
 func Prepare() {
 	switch flag {
 	case "s2p":
-		stageList = parseJSON(stageURL, stagePath)
-		stage = aquireID("https://"+stageURL+"/"+site+"/", stageList)
-		backupDB(stagePath)
+		fromPath, fromURL = stagePath, stageURL
+		stageList = parseJSON(fromURL, fromPath)
+		stageObj = aquireID("https://"+fromURL+"/"+site+"/", stageList)
+		first(stageObj)
+		toPath, toURL = prodPath, prodURL
+		prodList = parseJSON(toURL, toPath)
+		prodObj = aquireID("https://"+toURL+"/"+site+"/", prodList)
+		second(stageObj, prodObj, toPath)
 	case "p2s":
-		prodList = parseJSON(prodURL, prodPath)
-		prod = aquireID("https://"+prodURL+"/"+site+"/", prodList)
-		backupDB(prodPath)
+		fromPath, fromURL = prodPath, prodURL
+		prodList = parseJSON(fromURL, fromPath)
+		prodObj = aquireID("https://"+fromURL+"/"+site+"/", prodList)
+		first(prodObj)
+		toPath, toURL = stagePath, stageURL
+		stageList = parseJSON(toURL, toPath)
+		stageObj = aquireID("https://"+toURL+"/"+site+"/", stageList)
+		second(prodObj, stageObj, toPath)
 	default:
-		testList = parseJSON(testURL, testPATH)
-		test = aquireID("http://test.engage.gov.bc.ca/"+site+"/", testList)
-		exportDB(test.URL)
-		flush()
+		testList = parseJSON(testURL, testPath)
+		testObj = aquireID("http://test.engage.gov.bc.ca/"+site+"/", testList)
+		first(testObj)
+		// second(testObj, testPath)
 	}
+}
+
+// Run the first few functions up to the new site creation
+func first(from Blog) {
+	exportDB(from.URL)
+	exportUsers(from.URL, fromPath)
+	createSite("https://"+toURL+"/"+site+"/", site, adminEmail)
+}
+
+// Run the remaining functions after being able to grab the new site ID
+func second(from, to Blog, path string) {
+	backupDB(path)
+	replaceIDs(from.BlogID, to.BlogID)
+	importDB()
+	linkFix(from.BlogID, to.BlogID)
+	assetCopy(fromPath, from.BlogID, toPath, to.BlogID)
+	folderRef(toURL, from.BlogID, to.BlogID)
+	httpFind(toURL)
+	remap(toURL)
+	flush()
 }
 
 // Query WordPress for a list of all sites and map the json data to a struct array
@@ -81,16 +112,11 @@ func exportDB(furl string) {
 
 // Create a user export file
 func exportUsers(furl, path string) {
-	exec.Command("/data/scripts/user_export.py", "-p", "/data/www-app/"+path+"/current/web/wp", "-u", furl, "-o", "/data/temp/"+site+".json").Run()
+	exec.Command("/bin/bash", "-c", "/data/scripts/user_export.py", "-p", "/data/www-app/"+path+"/current/web/wp", "-u", furl, "-o", "/data/temp/"+site+".json").Run()
 }
 
-func createSite(url, title, email string) {
-	exec.Command("wp", "site", "create", "--url="+url, "--title="+title, "--email="+email, "--quiet").Run()
-}
-
-// Import the data
-func importDB() {
-	exec.Command("wp", "db", "import", "/data/temp/"+site+".sql", "--quiet").Run()
+func createSite(turl, title, email string) {
+	exec.Command("wp", "site", "create", "--url="+turl, "--title="+title, "--email="+email, "--quiet").Run()
 }
 
 // Backup the database
@@ -103,9 +129,9 @@ func replaceIDs(fid, tid string) {
 	exec.Command("sed", "-i", "'s/wp_"+fid+"_/wp_"+tid+"_/g'", "/data/temp/"+site+".sql").Run()
 }
 
-// Copy the site assets over
-func assetCopy(fpath, fid, tpath, tid string) {
-	exec.Command("rsync", "-a", "/data/www-assets/"+fpath+"/uploads/sites/"+fid+"/", "/data/www-assets/"+tpath+"/uploads/sites/"+tid+"/").Run()
+// Import the data
+func importDB() {
+	exec.Command("wp", "db", "import", "/data/temp/"+site+".sql", "--quiet").Run()
 }
 
 // Correct the links with search-replace
@@ -113,9 +139,9 @@ func linkFix(furl, turl string) {
 	exec.Command("wp", "search-replace", "--url="+turl, "--all-tables-with-prefix", furl, turl, "--quiet").Run()
 }
 
-// Catch any lingering http addresses
-func httpFind(turl string) {
-	exec.Command("wp", "search-replace", "--url="+turl, "--all-tables-with-prefix", "http://", "https://", "--quiet").Run()
+// Copy the site assets over
+func assetCopy(fpath, fid, tpath, tid string) {
+	exec.Command("rsync", "-a", "/data/www-assets/"+fpath+"/uploads/sites/"+fid+"/", "/data/www-assets/"+tpath+"/uploads/sites/"+tid+"/").Run()
 }
 
 // Correct the uploads folder references
@@ -123,9 +149,14 @@ func folderRef(turl, fid, tid string) {
 	exec.Command("wp", "search-replace", "--url="+turl, "--all-tables-with-prefix", "app/uploads/sites/"+fid, "app/uploads/sites/"+tid, "--quiet").Run()
 }
 
+// Catch any lingering http addresses
+func httpFind(turl string) {
+	exec.Command("wp", "search-replace", "--url="+turl, "--all-tables-with-prefix", "http://", "https://", "--quiet").Run()
+}
+
 // Remap the users to match their new ID
 func remap(turl string) {
-	exec.Command("user_import.py", "-p", "current/web/wp", "-u", turl, "-i ", "/data/temp/"+site+".json", "--quiet").Run()
+	exec.Command("/bin/bash", "-c", "/data/scripts/user_import.py", "-p", "current/web/wp", "-u", turl, "-i ", "/data/temp/"+site+".json").Run()
 }
 
 // Flush the WordPress cache
